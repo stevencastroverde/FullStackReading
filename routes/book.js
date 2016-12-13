e686009597a8e4f6379ceb0a751b6578968458c6
Steven Castroverde <stevencastroverde@Stevens-MacBook-Air.local>
2016-12-13 14:38:48 -0700

    initial

---
+++ routes/book.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+const knex = require('../knexconnect')
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  knex('book')
+  .select('Title', 'Page_Count as Number of Pages')
+  .then((books) =>{
+    res.render('viewall', {data:books});
+
+
+  })
+});
+
+// router.get('/', function(req,res){
+//   knex('book')
+//   .where('ID', req.params.id)
+//   .then((bookId) => {
+//     console.log(bookId)
+//     res.render('onebook', bookId);
+//
+//
+//   })
+//
+//
+//
+//
+// })
+
+
+
+module.exports = router;
